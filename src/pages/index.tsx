56750132a62a8ff2a19367a3d6307eb462731457
Muhammed Rede Maklad <mohammedmaklad97@gmail.com>
2024-07-07 15:42:11 +0300

    enable useQuery hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import axiosInstance from "../config/axios.config";
-import { ITodo } from "../interfaces";
+// import { ITodo } from "../interfaces";
 import Button from "../components/ui/Button";
-
-const storageKey = "loggedInUser";
-const userData = JSON.parse(`${localStorage.getItem(storageKey)}`);
+import { useQuery } from "@tanstack/react-query";
+import { ITodo } from "../interfaces";
 
 const HomePage = () => {
+  const storageKey = "loggedInUser";
+  const userData = JSON.parse(`${localStorage.getItem(storageKey)}`);
+
+  const { isLoading, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get("/users/me?populate=todos", {
+        headers: { Authorization: `Bearer ${userData.jwt}` },
+      });
+      return data;
+    },
+  });
+
+  console.log(data);
+
+  /*
   const [todoList, setTodos] = useState<ITodo[]>([]);
   useEffect(() => {
     try {
@@ -25,28 +40,36 @@ const HomePage = () => {
       console.log("Finally");
     }
   }, [userData.jwt]);
-
-  // Render Todos
-  console.log(userData);
-  const renderTodos = todoList.map((todo) => {
-    return (
-      <div
-        key={todo.id}
-        className="flex flex-row items-center justify-between space-y-2 p-2 rounded-md odd:bg-gray-200"
-      >
-        <p className="font-semibold max-w-full">{todo.title}</p>
-        <div className="flex space-x-3 items-center justify-center max-w-full">
-          <Button size={"sm"}>Edit</Button>
-          <Button variant={"danger"} size={"sm"}>
-            Delete
-          </Button>
-        </div>
-      </div>
-    );
-  });
+*/ // Render Todos
+  // console.log(userData);
+  if (isLoading) return <h3 className="">Loading...</h3>;
+  // if (error) return <h3 className="">Something Went Wrong Try Again...</h3>;
   return (
     <>
-      <div className="flex flex-col mx-auto max-w-md">{renderTodos}</div>
+      <div className="flex flex-col mx-auto max-w-md text-center">
+        {data.todos.length == 0 ? (
+          <h3 className="text-indigo-600 font-bold text-3xl">
+            NO TODOS YET...!
+          </h3>
+        ) : (
+          data.todos.map((todo: ITodo) => {
+            return (
+              <div
+                className="flex flex-row items-center justify-between space-y-2 p-2 rounded-md odd:bg-gray-200"
+                key={todo.id}
+              >
+                <p className="font-semibold max-w-full">{todo.title}</p>
+                <div className="flex space-x-3 items-center justify-center max-w-full">
+                  <Button size={"sm"}>Edit</Button>
+                  <Button variant={"danger"} size={"sm"}>
+                    Delete
+                  </Button>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
     </>
   );
 };
