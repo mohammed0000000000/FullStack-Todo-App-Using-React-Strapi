1dc231cfe495bea704b3271e508ae9419d7fe153
Muhammed Rede Maklad <mohammedmaklad97@gmail.com>
2024-07-09 16:45:37 +0300

    API pagination

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,7 +1,7 @@
 import TodoSkeleton from "../components/TodoSkeleton";
 import useCustomQuery from "../hooks/useCustomQuery";
 import Paginator from "../components/ui/Pageinator";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface ITodo {
   id: number;
@@ -14,6 +14,8 @@ const TodosPage = () => {
   // Pagination
 
   const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [sortedBy, setSortedBy] = useState<string>("Latest");
 
   const onClickPrev = () => {
     setPage((prev) => prev - 1);
@@ -25,8 +27,8 @@ const TodosPage = () => {
   const storageKey = "loggedInUser";
   const userData = JSON.parse(`${localStorage.getItem(storageKey)}`);
   const { isLoading, data, isFetching } = useCustomQuery({
-    queryKey: [`todo-page-${page}`],
-    url: `/todos?pagination[pageSize]=25&pagination[page]=${page}`,
+    queryKey: [`todo-page-${page}`, `${pageSize}`, ` ${sortedBy}`],
+    url: `/todos?pagination[pageSize]=${pageSize}&pagination[page]=${page}&sort=createdAt:${sortedBy}`,
     config: {
       headers: {
         Authorization: `Bearer ${userData.jwt}`,
@@ -50,6 +52,38 @@ const TodosPage = () => {
   return (
     <>
       <div className="flex flex-col mx-auto max-w-md text-center space-y-3">
+        <div className="flex justify-end space-x-2">
+          <select
+            name="sortedBy"
+            className="border border-gray-500 py-2 px-4 rounded-lg"
+            onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+              setSortedBy(event.target.value);
+            }}
+            value={sortedBy}
+          >
+            <option value="" disabled selected>
+              Sort By
+            </option>
+            <option value="asc">Oldest</option>
+            <option value="desc">Latest </option>
+          </select>
+          <select
+            name="pageSize"
+            id=""
+            className="border border-gray-500 py-1 px-4 rounded-lg"
+            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+              setPageSize(Number(event.target.value))
+            }
+          >
+            <option value={pageSize} disabled selected className="text-black">
+              Page Size
+            </option>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </div>
         {data?.data?.length == 0 ? (
           <h3 className="text-indigo-600 font-bold text-3xl">
             NO TODOS YET...!
@@ -69,15 +103,15 @@ const TodosPage = () => {
             );
           })
         )}
+        <Paginator
+          page={page}
+          pageCount={pageCount}
+          onClickPrev={onClickPrev}
+          isLoading={isLoading || isFetching}
+          total={total}
+          onClickNext={onClickNext}
+        ></Paginator>
       </div>
-      <Paginator
-        page={page}
-        pageCount={pageCount}
-        onClickPrev={onClickPrev}
-        isLoading={isLoading || isFetching}
-        total={total}
-        onClickNext={onClickNext}
-      ></Paginator>
     </>
   );
 };
